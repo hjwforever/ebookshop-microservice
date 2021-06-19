f4dcbc6211fc3b3cea0eda40a7d375a78637cec4
Chen_Fuxin <1183458938@qq.com>
2021-06-19 14:22:49 +0800

    loadbalancer、consumer、provider、eureka功能实现

---
+++ gateway-service/src/test/java/com/aruoxi/ebookshop/gateway/GatewayServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.aruoxi.ebookshop.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
+
--- shop-service/src/main/java/com/aruoxi/microservice/shop/config/SecurityConfig.java
+++
@@ -1,5 +0,0 @@
-package com.aruoxi.microservice.shop.config;
-
-public class SecurityConfig {
-
-}
